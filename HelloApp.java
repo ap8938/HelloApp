05a5ab77eed3322cfb222f1e54b4fed522ffebf9
ap8938 <ap8938@srmist.edu.in>
2026-03-18 14:38:47 +0530

    Added UC4

--- HelloApp.java
+++ HelloApp.java
@@ -1,4 +1,4 @@
-public class HelloApp {
+/*public class HelloApp {
     public static void main(String[] args) {
 		String name="World";
         if (args.length > 0) {
@@ -8,5 +8,22 @@ public class HelloApp {
 		else {
 			System.out.println("Hello, "+name+"!");
 		}
+    }
+}*/
+
+public class HelloApp {
+    public static void main(String[] args){
+        String name = "World";
+        if (args.length > 0) {
+            StringBuilder nameBuilder = new StringBuilder();
+            for (int i=0; i<args.length; i++){
+                nameBuilder.append(args[i]);
+                if (i<args.length-1){
+                    nameBuilder.append(", ");
+                }
+            }
+            name = nameBuilder.toString();
+        }
+        System.out.println("Hello, " + name + "!");
     }
 }
