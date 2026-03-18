fd9bca1adb493004d060cb98f55ab4be1ceeadde
ap8938 <ap8938@srmist.edu.in>
2026-03-18 14:49:53 +0530

    Added UC5

--- HelloApp.java
+++ HelloApp.java
@@ -1,16 +1,18 @@
-public class HelloApp {
+public class HelloApp{
     public static void main(String[] args){
         String name = "World";
-        if (args.length > 0) {
+        if (args.length>0){
             StringBuilder nameBuilder = new StringBuilder();
-            for (int i=0; i<args.length; i++){
-                nameBuilder.append(args[i]);
-                if (i<args.length-1){
+            boolean first = true;
+            for (String n : args){
+                if (!first) {
                     nameBuilder.append(", ");
                 }
+                nameBuilder.append(n);
+                first = false;
             }
             name = nameBuilder.toString();
         }
-        System.out.println("Hello, " + name + "!");
+        System.out.println("Hello, "+name+"!");
     }
 }
